231977b7f6ff0edf6f6e00e1041a68320dcb591c
Kurilo Denis <webprogram2017@mail.ru>
2020-06-05 18:37:49 +0300

    Исправил ошибку budgetDay

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
-let money = 35000,
+let money = 40000,
 	income = 'фриланс',
 	addExpenses = 'Интернет, Коммуналка, Спорт зал, Машина',
 	deposit = true,
 	mission = 500000,
 	period = 12,
-	budgetDay;
+	budgetDay = Math.round(money / 30);
 
 console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев и Цель заработать ${mission} рублей”`);
 console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay = money + ' руб');
+console.log(budgetDay + ' руб');
