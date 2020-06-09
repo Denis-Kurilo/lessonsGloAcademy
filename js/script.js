8d9763bf3bdd4ca83be6e657d017f7323fcbb935
Kurilo Denis <webprogram2017@mail.ru>
2020-06-09 20:32:57 +0300

    Исправил ошибки в lesson03

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 let money = +prompt('Ваш месячный доход?',''),
-	income = 'фриланс',
 	addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
 	deposit = confirm('Есть ли у вас депозит в банке?'),
 	mission = 500000,
-	expenses1 = +prompt('Введите обязательную статью расходов?'),
-	expenses2 = +prompt('Введите обязательную статью расходов?'),
-	// amount1 = prompt('Во сколько это обойдется?'),
-	// amount2 = prompt('Во сколько это обойдется?'),
-	budgetMonth = money - expenses1 - expenses2,
+	expenses1 = prompt('Введите обязательную статью расходов?'),
+	expenses2 = prompt('Введите обязательную статью расходов?'),
+	amount1 = +prompt('Во сколько это обойдется?'),
+	amount2 = +prompt('Во сколько это обойдется?'),
+	budgetMonth = money - amount1 - amount2,
 	month = Math.ceil(mission / budgetMonth),
 	budgetDay = Math.floor(budgetMonth / 30);
 
-if(!Number(money), !Number(expenses1), !Number(expenses2)){
+if(!Number(money), !Number(amount1), !Number(amount2)){
 	alert('Вы не ввели число. Ведите числовое значение');
 }
 
-console.log(addExpenses.length);
-console.log(deposit);
-console.log(budgetMonth);
-console.log(`будет достигнута цель за ${month} месяцев.`);
-console.log(budgetDay);
+console.log(addExpenses.split(', '));
+console.log(typeof deposit);
+console.log(expenses1 + ' ' + amount1);
+console.log(expenses2 + ' ' + amount2);
+console.log('Бюджет на месяц ' + budgetMonth);
+console.log(`Будет достигнута цель за ${month} месяцев.`);
+console.log('Бюджет на месяц ' + budgetDay);
 
 if(budgetDay >= 1200){
 	console.log('У вас высокий уровень дохода');
