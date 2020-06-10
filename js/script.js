059bbe63a8d353c7b043e83efa3c7601653e372b
Denis Kurilo <kdb2015@mail.ru>
2020-06-10 16:13:36 +0300

    внес правки в lesson04

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+/*
+
+
+3) вывод budgetDay потерялся
+*/
+
 let money = +prompt('Ваш месячный доход?',''),
 	income = 'фриланс',
 	addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
 	deposit = confirm('Есть ли у вас депозит в банке?'),
 	mission = 500000,
-	expenses1 = prompt('Введите обязательную статью расходов?'),
-	expenses2 = prompt('Введите обязательную статью расходов?'),
-	amount1 = +prompt('Во сколько это обойдется?'),
-	amount2 = +prompt('Во сколько это обойдется?'),
+	expenses1 = prompt('Введите обязательную статью расходов?', 'квартплата'),
+	amount1 = +prompt('Во сколько это обойдется?', 5000),
+	expenses2 = prompt('Введите обязательную статью расходов?', 'авто'),
+	amount2 = +prompt('Во сколько это обойдется?', 3000),
 	accumulatedMonth = getAccumulatedMonth(money, amount1, amount2),
 	budgetDay = Math.floor(accumulatedMonth / 30);
 
@@ -37,7 +43,7 @@ let getStatusIncome = function(){
 		return ('Что то пошло не так');
 	}	
 }
-console.log(getStatusIncome());
+
 
 const getExpensesMonth = function(amount1, amount2){
 	return amount1 + amount2;
@@ -50,6 +56,10 @@ function getAccumulatedMonth(money, amount1, amount2) {
 console.log('Накопления за месяц ', accumulatedMonth)
 
 function getTargetMonth(mission, accumulatedMonth){
-	 return mission / accumulatedMonth;
+	 return Math.ceil(mission / accumulatedMonth);
 }
-console.log('Будет достигнута цель ', Math.ceil(getTargetMonth(mission, accumulatedMonth)));
+console.log('Будет достигнута цель ', getTargetMonth(mission, accumulatedMonth));
+console.log(getStatusIncome());
+
+
+
