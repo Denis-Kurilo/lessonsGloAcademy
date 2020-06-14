539ad2b1671fd7ee416b5d28c7f6aeaf28eae565
Kurilo Denis <webprogram2017@mail.ru>
2020-06-14 17:45:46 +0300

    add lesson06GameBot

--- js/script.js
+++ js/script.js
@@ -1,75 +1,34 @@
+let number,
+  count = 10;
 
-let isNumber = function(n){
-	return !isNaN(parseFloat(n)) && isFinite(n);
-};
+function randomInteger(min, max) {
+    return  Math.floor(Math.random() * (max - min)) + 1 
+    };
+number = randomInteger(1,100);
 
-let	income = 'фриланс',
- 	money,
-	addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-	deposit = confirm('Есть ли у вас депозит в банке?'),
-	mission = 500000,
-	expenses = [];
+function guessNum() {
+     let num;
 
+// console.log(number)
+     num = +prompt('Угадайте число от 1 до 100');
+     if(count == 1) {
+          return;
+     }
+     count--;
 
-function start(){
-	do{
-		money = +prompt('Ваш месячный доход?');
-	}while(!isNumber(money))
-};
-start();
-let expensesAmount = getExpensesMonth();
-let accumulatedMonth = getAccumulatedMonth();
-let budgetDay = Math.floor(accumulatedMonth / 30);
-
-let showTypeOf = function(data){
-	console.log(data, typeof(data))
-};
-
-let getStatusIncome = function(){
-	if(budgetDay >= 1200){
-		return ('У вас высокий уровень дохода');
-	} else if (budgetDay >= 600 && budgetDay < 1200){
-		return ('У вас средний уровень дохода');
-	} else if (budgetDay < 600){
-		return ('К сожалению у вас уровень дохода ниже среднего');
-	} else if (budgetDay <= 0){
-		return ('Что то пошло не так');
-	}	
+     if (num == number) {
+        alert('вы угадали! ');
+     } else if (num > number && num != '') {
+        alert('Вы ввели число больше! Осталось попыток: ' + count);
+        guessNum();
+     }else if (num < number && num != '') {
+        alert('Вы ввели число меньше! Осталось попыток: ' + count);
+        guessNum();
+     } else if (isNaN(parseFloat(num) && isFinite(num))) {
+        alert('Введите число');
+        guessNum();
+     }else{
+       alert('Завершить игру')
+     }
 }
-
-function getExpensesMonth(){
-	let sum = 0;
-	let cost;
-	for (let i = 0; i < 2; i++){
-		expenses[i] = prompt('Введите обязательную статью расходов?');
-		do{
-			cost = +prompt('Во сколько это обойдется?');	
-		}while(!isNumber(cost));
-		sum += cost;
-	}
-	return sum;
-	console.log(expenses);
-};
-
-function getAccumulatedMonth() {
-	return money - expensesAmount;
-}
-
-function getTargetMonth(){
-	 return Math.ceil(mission / accumulatedMonth);	 
-}
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log(addExpenses.toLowerCase().split(', '));
-console.log('Расходы за месяц', expensesAmount);
-console.log('Накопления за месяц ', accumulatedMonth);
-console.log(getStatusIncome());
-
-if(getTargetMonth() < 0 ){
- 	console.log('Цель не будет достигнута ' + getTargetMonth());
- } else {
- 	console.log('Будет достигнута цель ' + getTargetMonth()); 
- }
+guessNum();
