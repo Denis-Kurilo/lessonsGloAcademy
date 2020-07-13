7ef722c6074ecdbb8e7bb68c691286b11bc78a67
Kurilo Denis <kdb2015@mail.ru>
2020-07-14 00:55:03 +0300

    lesson24

--- lesson24/js/script.js
+++ lesson24/js/script.js
@@ -284,8 +284,14 @@ window.addEventListener('DOMContentLoaded', function(){
 	slider();
 
 	//calculate
-	const calculate = () =>{
-		const calcBlock = document.querySelector('.calc-block');
+	const calculate = (price = 100) =>{
+		const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector('.calc-type'),
+			calcSquare = document.querySelector('.calc-square'),
+			calcCount = document.querySelector('.calc-count'),
+			calcDay = document.querySelector('.calc-day'),
+			totalValue = document.getElementById('total');
+
 		calcBlock.addEventListener('input', (e) =>{
 			let target = e.target.closest('input');
 			if(target){
@@ -293,14 +299,45 @@ window.addEventListener('DOMContentLoaded', function(){
 				input = target.value = input.replace(/\D/g, '');
 			}
 		});
+
+		const countSum = () =>{
+			let total = 0,
+			countValue = 1,
+			dayValue = 1;
+			const typeValue = calcType.options[calcType.selectedIndex].value,
+				 squareValue = +calcSquare.value;
+			
+			if(calcCount.value > 1){
+				countValue += (calcCount.value -1) / 10;
+			}
+
+			if(calcDay.value && calcDay.value < 5){
+				dayValue *= 2;
+			} else if(calcDay.value && calcDay.value < 10){
+				dayValue *= 1.5;
+			}
+
+			if(typeValue && squareValue){
+				total = price * typeValue * squareValue * countValue * dayValue;
+			}
+			totalValue.textContent = total;
+		};
+
+		calcBlock.addEventListener('change', (event) =>{
+			const target = event.target;
+
+			if(target.matches('select') || target.matches('input')){
+				countSum();
+			}
+		});
 	}
-	calculate();
+	calculate(100);
 
 	//Our team
 	const team = () =>{
 		const command = document.getElementById('command');
 		command.addEventListener('mouseover', (e) =>{
-			let target = e.target;
+			const target = e.target;
 			if(target.closest('img')){
 				target.src = target.dataset.img;
 			}
